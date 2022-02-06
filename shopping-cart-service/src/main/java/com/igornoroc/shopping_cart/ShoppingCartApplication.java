b7e9563f4af91a9ff29da303243f6fbffc7f98f0
Igor <workuglis@gmail.com>
2022-02-06 22:43:34 +0300

    fixed bags : start config-service/ registry / gateway (changed dependencies for each service).

--- shopping-cart-service/src/main/java/com/igornoroc/shopping_cart/ShoppingCartApplication.java
+++ shopping-cart-service/src/main/java/com/igornoroc/shopping_cart/ShoppingCartApplication.java
@@ -1,7 +1,11 @@
 package com.igornoroc.shopping_cart;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ShoppingCartApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(ShoppingCartApplication.class, args);
     }
 }
